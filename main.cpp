9e374093d221d5e5727a454ee9989992e19c9772
Igor Olewicz <94185939+CZIWELLO@users.noreply.github.com>
2026-04-21 17:22:25 +0200

    baza programu
    stworzono strukturę plików i bazowe dane

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "helpers.h"
+#include "introspektywne.h"
+#include "quicksort.h"
+#include "scalanie.h"
+#include <vector>
+
+int main() {
+    
+    unsigned int wielkosci[] = {10000, 50000, 100000, 500000, 1000000};
+    const u_int8_t liczba_testow = 100;
+    float poziom_posortowania[] = {0.0, 0.25, 0.5, 0.75, 0.95, 0.99, 0.997, -1.0};
+
+    
+
+
+    return 0;
+}
